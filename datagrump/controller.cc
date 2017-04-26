2e6187ddc9d3b321f755d36995de6d0a5a61a399
cs244 <tlanham@cs.stanford.edu>
2017-04-26 00:57:49 -0700

    cleanup for delay trigger warmup c

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -18,12 +18,6 @@ Controller::Controller( const bool debug)
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size( void )
 {
-
-  if (wsz > 200)
-    wsz = 80;
-  else if (wsz < 20)
-    wsz = 40;
-
   /* Default: fixed window size of 100 outstanding datagrams */
   unsigned int the_window_size = (unsigned int) wsz;
 
@@ -42,12 +36,6 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
                                     /* in milliseconds */
 {
   /* Default: take no action */
-  if ((send_timestamp - last_sent) > (uint) rtt){
-    if ( debug_ )  cerr << "__DEBUG__: halving window " << "rtt= " << rtt << endl;
-    // TODO wsz = wsz/2;
-  }
-
-  last_sent = timestamp_ms();
 
   if ( debug_ ) {
     cerr << "At time " << send_timestamp
@@ -75,31 +63,16 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 	 << endl;
   }
 
-  if (rtt - old_rtt > 0) {
-    wsz -= (((float)rtt - (float)old_rtt) / (float)old_rtt) * wsz;
-    wsz = wsz < 0 ? 0 : wsz;
-    cerr << "__DEBUG__: "<< "(" << wsz <<")"  << " decreasing: " << (((float)rtt - (float)old_rtt) / (float)old_rtt) * wsz << endl;
-  } else {
-    wsz += (((float)old_rtt - (float)rtt) / (float)rtt) * wsz;
-    cerr << "__DEBUG__: "<< "(" << wsz <<")"  << " increasing: " << (((float)rtt - (float)old_rtt) / (float)old_rtt) * wsz << endl;
-  }
-
   rtt = (timestamp_ack_received - send_timestamp_acked);
   if (rtt < RTT_EXPAND_THRESH) 
-    {
-      wsz++;
-      if ( debug_ ) cerr << "__DEBUG__: expanding win " << wsz << " | rtt: " << rtt << endl;
-    } 
+    wsz++;
   else if (rtt > RTT_CONTRACT_THRESH) 
-    {
-      wsz /= 2;
-      if ( debug_ ) cerr << "__DEBUG__: contracting win " << wsz << " | rtt: " << rtt << endl;
-    }
+    wsz--;
 }
 
 /* How long to wait (in milliseconds) if there are no acks
    before sending one more datagram */
 unsigned int Controller::timeout_ms( void )
 {
-  return rtt; /* timeout of one second */
+  return 1000; /* timeout of one second */
 }
